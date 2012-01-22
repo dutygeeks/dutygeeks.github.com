4a3ee16289a0a47e758dfd8fd4b2a1bbbeeb6311
Seiichi <dutygeeks@gmail.com>
2012-01-22 09:33:22 +0900

    my filter is fail

--- lib/filters/smusher.rb
+++ lib/filters/smusher.rb
@@ -1,1 +1,22 @@
-# let's get started.
+class Smusher < Nanoc3::Filter
+  
+  
+  identifier :smusher
+  type       :binary
+  
+  # "output_finename"
+  
+  def run(filename, params={})
+    system(
+      'smusher',
+      filename
+    )
+  end
+  
+  # didn't work too
+  
+  # def run(content, params={})
+  #   ::Smusher.optimize_images_in_folder(content)
+  # end
+  
+end
